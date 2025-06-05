3bb58041f28d2d070b2766f2ba8748d14ad0d44c
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-05 23:09:15 +0530

    feat(loader): add spinner for loading state

--- components/saveUsername/saveUsername.tsx
+++ components/saveUsername/saveUsername.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import React, { FormEvent, use, useState } from 'react'
+import React, { FormEvent, use, useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import z from 'zod'
 import axios from 'axios'
+import Spinner from '../spinner'
 
 const formSchema = z.object({
     username: z.string()
@@ -22,18 +23,45 @@ const formSchema = z.object({
 function SaveUsername({ username }: { username: string }) {
     const [email, setEmail] = useState<string>('')
     const [error, setError] = useState<string | null>('')
+    const [loading, setLoading] = useState<Boolean>(false)
+
+    useEffect(() => {
+        const emailFromlocalStorage: string | null = localStorage.getItem('email')
+        if (emailFromlocalStorage) {
+            setEmail(emailFromlocalStorage)
+        }
+    },[])
+
 
     const hanldesaveUser = async (e: FormEvent) => {
         e.preventDefault()
         setError(null)
-        const result = formSchema.safeParse({ username, email })
+        setLoading(true)
 
-        if(!result.success){
+        const result = formSchema.safeParse({ username, email })
+        if (!result.success) {
             setError(result.error.errors[0].message)
+            setLoading(false)
             return
         }
 
-        const res = await axios.post('/api/user/create',{})
+        try {
+            const avatar = localStorage.getItem('avatar_url')
+            const res = await axios.post('/api/user/create', { username, email, avatar }, {
+                validateStatus: () => true
+            })
+
+            if (res.request.responseURL && res.request.responseURL.includes('/main')) {
+                window.location.href = res.request.responseURL
+            } else {
+                console.log('Response:', res.data)
+            }
+
+        } catch (err) {
+            setError('Something went wrong')
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
@@ -42,7 +70,7 @@ function SaveUsername({ username }: { username: string }) {
                 <CardTitle>Create Account</CardTitle>
             </CardHeader>
             <CardContent>
-                <form className="space-y-4" >
+                <form className="space-y-4" onSubmit={hanldesaveUser} >
                     <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="username">Username</Label>
                         <Input id="username" name="username" value={username} disabled />
@@ -50,10 +78,10 @@ function SaveUsername({ username }: { username: string }) {
 
                     <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="email">Email</Label>
-                        <Input id="email" name="email" type="email" placeholder="you@example.com" />
+                        <Input id="email" name="email" type="email" placeholder="you@example.com" value={email} onChange={e => setEmail(e.target.value)} />
                     </div>
 
-                    <Button type="submit" className="w-full cursor-pointer">Save</Button>
+                    <Button type="submit" className="w-full cursor-pointer">{loading ? <Spinner width="32" height="32"/> : 'save'}</Button>
                 </form>
             </CardContent>
         </Card>
