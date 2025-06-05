9ec3a13f1439a382d2f446c32dd5e498dd5ab89e
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-05 23:08:06 +0530

    fix: fixed a username bug

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -4,11 +4,9 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(req: NextRequest) {
 
     try {
-
         const { username, email, avatar } = await req.json()
-
-        if (!username)
-            return NextResponse.json({ ok: false, msg: 'username is missing' }, { status: 404 })
+        if (!username || !email)
+            return NextResponse.json({ ok: false, msg: 'username or email is missing' }, { status: 404 })
 
         const user = await prisma.user.create({
             data: {
@@ -19,10 +17,9 @@ export async function POST(req: NextRequest) {
             }
         })
 
-        return NextResponse.json({ ok: true, user }, { status: 200 })
+        return NextResponse.redirect(new URL('/main', req.url))
 
     } catch (error: any) {
-
         return NextResponse.json({ ok: false, error: 'Internal Server Error' }, { status: 500 })
     }
 
