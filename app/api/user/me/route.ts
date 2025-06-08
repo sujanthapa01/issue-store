2a205dbc7553eb24e741d86657cfb91234f94955
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-08 16:14:34 +0530

    feat: added getUserConetxt for user data

--- app/api/user/me/route.ts
+++ app/api/user/me/route.ts
@@ -2,30 +2,22 @@ import { prisma } from '@/lib/prismaClient'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
-  try {
-    const { username, userId } = await req.json();
+    try {
+        const { email } = await req.json();
 
-    let user
 
-    if (username || userId) {
-      user = await prisma.user.findUnique({
-        where: { username },
-      });
+        if (!email) {
+            return NextResponse.json({ ok: false, msg: "userAuthId is missing" }, { status: 404 })
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id: email }
+        })
+
+        return NextResponse.json({ ok: true, user: user }, { status: 200 })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
-
-    if (!user && userId) {
-      user = await prisma.user.findUnique({
-        where: { id: userId },
-      });
-    }
-
-    if (!user) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json(user, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-  }
 }
