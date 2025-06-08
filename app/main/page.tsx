03715081e767c97b1fb811d5023d05957158d84b
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-08 23:47:35 +0530

    👀 chore: add logs to monitor state

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,10 +1,32 @@
 'use client'
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
+
 const Page = () => {
 
+  const [email, setEmail] = useState<string | null>('')
+  const [username, setUsername] = useState<string | null>('')
+
+  const fetchUser = useCallback(async () => {
+    const email = localStorage.getItem('email')
+
+    const res = await axios.post('/api/user/me', { email })
+    console.log(res.data.user.id)
+    if (res) {
+      setEmail(res.data.user.email)
+      setUsername(res.data.user.username)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchUser()
+  }, [fetchUser])
+
   return (
-    <div className='flex justify-center items-center h-screen'>
-  main page
+    <div className='flex justify-center items-center h-screen text-black'>
+
+      <p>{email}</p>
+      <p>{username}</p>
     </div>
   )
 }
