731e84bc954897eba5499526b4b1f44d7e4fd943
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-09 17:26:14 +0530

    commit

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,34 +1,11 @@
 'use client'
-import React, { useCallback, useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const Page = () => {
-
-  const [email, setEmail] = useState<string | null>('')
-  const [username, setUsername] = useState<string | null>('')
-
-  const fetchUser = useCallback(async () => {
-    const email = localStorage.getItem('email')
-
-    const res = await axios.post('/api/user/me', { email })
-    console.log(res.data.user.id)
-    if (res) {
-      setEmail(res.data.user.email)
-      setUsername(res.data.user.username)
-    }
-  }, [])
-
-  useEffect(() => {
-    fetchUser()
-  }, [fetchUser])
-
+function page() {
   return (
-    <div className='flex justify-center items-center h-screen text-black'>
 
-      <p>{email}</p>
-      <p>{username}</p>
-    </div>
+    <div>page</div>
   )
 }
 
-export default Page
+export default page
