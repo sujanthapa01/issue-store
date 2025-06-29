612b5d6a00fa2c709cad9c1a08f526aa8c86f40a
sujanthapa01 <sujanthapa4001@gmail.com>
2025-06-29 16:41:10 +0530

    refactor(api)

--- app/api/feed/route.ts
+++ app/api/feed/route.ts
@@ -1,21 +1,27 @@
+// app/api/feed/route.ts
+
 import { prisma } from "@/lib/prismaClient"
 import { NextRequest, NextResponse } from "next/server"
 
+export async function GET(req: NextRequest) {
+  try {
+    const feed = await prisma.repository.findMany({
+      where: { isPrivate: false },
+      orderBy: { createdAt: "desc" },
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+        issues: true,
+      },
+    })
 
-export async function GET(req: NextRequest, res: NextResponse) {
-
-    try {
-
-        const feed = await prisma.repository.findMany({
-            where: { isPrivate: false },
-            orderBy: { createdAt: "desc" },
-        })
-
-        return NextResponse.json({ ok: true, feed }, { status: 200 })
-
-
-    } catch (error) {
-
-        return NextResponse.json({ ok: false, msg: "Internal server error" })
-    }
+    return NextResponse.json({ ok: true, feed }, { status: 200 })
+  } catch (error) {
+  
+    return NextResponse.json({ ok: false, msg: "Internal server error" }, { status: 500 })
+  }
 }
