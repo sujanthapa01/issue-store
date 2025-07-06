f0f3e9358364faccc216f8d605df05c7790ac815
sujanthapa01 <sujanthapa4001@gmail.com>
2025-07-06 23:56:44 +0530

    update

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -9,6 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'react/jsx-key': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/no-unknown-property': 'off',
+    },
+  },
+];
 
 export default eslintConfig;
