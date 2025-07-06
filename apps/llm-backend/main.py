c26502cb07150bc7353746e2c957a0c78c5b3fe7
sujanthapa01 <sujanthapa4001@gmail.com>
2025-07-06 15:46:37 +0530

    refactor: test

--- apps/llm-backend/main.py
+++ apps/llm-backend/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
 
 app = FastAPI()
 
-class Prompt(BaseModel):
-    text: str
-
-@app.post("/generate")
-def generate(prompt: Prompt):
-    return {"response": f"Echo: {prompt.text}"}
+@app.post("/")
+def root():
+    return {"response": "helo from python server using fastapi"}
